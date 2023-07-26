557cb633da6eba95b5104cdb6d1f32777122190b
pylakey <pylakey@protonmail.com>
2023-07-26 18:44:26 +0800

    Adopted ChatPermissions and ChatBannedRights for new layers

--- pyrogram/types/user_and_chats/chat_permissions.py
+++ pyrogram/types/user_and_chats/chat_permissions.py
@@ -15,6 +15,7 @@
 #
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
+from warnings import warn
 
 from pyrogram import raw
 from ..object import Object
@@ -24,56 +25,113 @@ class ChatPermissions(Object):
     """Describes actions that a non-administrator user is allowed to take in a chat.
 
     Parameters:
+        can_view_messages (``bool``, *optional*):
+            True, if the user is allowed to view messages.
+
         can_send_messages (``bool``, *optional*):
             True, if the user is allowed to send text messages, contacts, locations and venues.
 
-        can_send_media_messages (``bool``, *optional*):
-            True, if the user is allowed to send audios, documents, photos, videos, video notes and voice notes.
-            Implies *can_send_messages*.
+        can_send_messages (``bool``, *optional*):
+            True, if the user is allowed to send messages
 
-        can_send_other_messages (``bool``, *optional*):
-            True, if the user is allowed to send animations, games, stickers and use inline bots.
-            Implies *can_send_media_messages*.
+        can_send_media (``bool``, *optional*):
+            True, if the user is allowed to send any media
+
+        can_send_stickers (``bool``, *optional*):
+            True, if the user is allowed to send stickers
+
+        can_send_gifs (``bool``, *optional*):
+            True, if the user is allowed to send gifs
+
+        can_send_games (``bool``, *optional*):
+            True, if the user is allowed to send games
+
+        can_send_inline (``bool``, *optional*):
+            True, if the user is allowed to use inline bots
+
+        can_embed_links (``bool``, *optional*):
+            True, if the user is allowed to embed links in the messages
 
         can_send_polls (``bool``, *optional*):
-            True, if the user is allowed to send polls.
-            Implies can_send_messages
-
-        can_add_web_page_previews (``bool``, *optional*):
-            True, if the user is allowed to add web page previews to their messages.
-            Implies *can_send_media_messages*.
+            True, if the user is allowed to send polls
 
         can_change_info (``bool``, *optional*):
-            True, if the user is allowed to change the chat title, photo and other settings.
-            Ignored in public supergroups
+            True, if the user is allowed to change the description of a supergroup/chat
 
         can_invite_users (``bool``, *optional*):
-            True, if the user is allowed to invite new users to the chat.
+            True, if the user is allowed to invite users
 
         can_pin_messages (``bool``, *optional*):
-            True, if the user is allowed to pin messages.
-            Ignored in public supergroups.
+            True, if the user is allowed to pin messages
+
+        can_manage_topics (``bool``, *optional*):
+            True, if the user is allowed to create, delete or modify forum topics
+
+        can_send_photos (``bool``, *optional*):
+            True, if the user is allowed to send photos
+
+        can_send_videos (``bool``, *optional*):
+            True, if the user is allowed to send videos
+
+        can_send_roundvideos (``bool``, *optional*):
+            True, if the user is allowed to send round videos
+
+        can_send_audios (``bool``, *optional*):
+            True, if the user is allowed to send audio files
+
+        can_send_voices (``bool``, *optional*):
+            True, if the user is allowed to send voice messages
+
+        can_send_docs (``bool``, *optional*):
+            True, if the user is allowed to send documents
+
+        can_send_plain (``bool``, *optional*):
+            True, if the user is allowed to send text messages
+
     """
 
     def __init__(
-        self,
-        *,
-        can_send_messages: bool = None,  # Text, contacts, locations and venues
-        can_send_media_messages: bool = None,  # Audio files, documents, photos, videos, video notes and voice notes
-        can_send_other_messages: bool = None,  # Stickers, animations, games, inline bots
-        can_send_polls: bool = None,
-        can_add_web_page_previews: bool = None,
-        can_change_info: bool = None,
-        can_invite_users: bool = None,
-        can_pin_messages: bool = None
+            self,
+            *,
+            can_view_messages: bool = None,
+            can_send_messages: bool = None,
+            can_send_media: bool = None,
+            can_send_stickers: bool = None,
+            can_send_gifs: bool = None,
+            can_send_games: bool = None,
+            can_send_inline: bool = None,
+            can_embed_links: bool = None,
+            can_send_polls: bool = None,
+            can_change_info: bool = None,
+            can_invite_users: bool = None,
+            can_pin_messages: bool = None,
+            can_manage_topics: bool = None,
+            can_send_photos: bool = None,
+            can_send_videos: bool = None,
+            can_send_roundvideos: bool = None,
+            can_send_audios: bool = None,
+            can_send_voices: bool = None,
+            can_send_docs: bool = None,
+            can_send_plain: bool = None,
     ):
         super().__init__(None)
-
         self.can_send_messages = can_send_messages
-        self.can_send_media_messages = can_send_media_messages
-        self.can_send_other_messages = can_send_other_messages
+        self.can_send_media = can_send_media
+        self.can_send_stickers = can_send_stickers
+        self.can_send_gifs = can_send_gifs
+        self.can_send_games = can_send_games
+        self.can_send_inline = can_send_inline
         self.can_send_polls = can_send_polls
-        self.can_add_web_page_previews = can_add_web_page_previews
+        self.can_send_photos = can_send_photos
+        self.can_send_videos = can_send_videos
+        self.can_send_roundvideos = can_send_roundvideos
+        self.can_send_audios = can_send_audios
+        self.can_send_voices = can_send_voices
+        self.can_send_docs = can_send_docs
+        self.can_send_plain = can_send_plain
+        self.can_view_messages = can_view_messages
+        self.can_embed_links = can_embed_links
+        self.can_manage_topics = can_manage_topics
         self.can_change_info = can_change_info
         self.can_invite_users = can_invite_users
         self.can_pin_messages = can_pin_messages
@@ -82,17 +140,60 @@ class ChatPermissions(Object):
     def _parse(denied_permissions: "raw.base.ChatBannedRights") -> "ChatPermissions":
         if isinstance(denied_permissions, raw.types.ChatBannedRights):
             return ChatPermissions(
+                can_view_messages=not denied_permissions.view_messages,
                 can_send_messages=not denied_permissions.send_messages,
-                can_send_media_messages=not denied_permissions.send_media,
-                can_send_other_messages=any([
-                    not denied_permissions.send_stickers,
-                    not denied_permissions.send_gifs,
-                    not denied_permissions.send_games,
-                    not denied_permissions.send_inline
-                ]),
-                can_add_web_page_previews=not denied_permissions.embed_links,
+                can_send_media=not denied_permissions.send_media,
+                can_send_stickers=not denied_permissions.send_stickers,
+                can_send_gifs=not denied_permissions.send_gifs,
+                can_send_games=not denied_permissions.send_games,
+                can_send_inline=not denied_permissions.send_inline,
+                can_embed_links=not denied_permissions.embed_links,
                 can_send_polls=not denied_permissions.send_polls,
                 can_change_info=not denied_permissions.change_info,
                 can_invite_users=not denied_permissions.invite_users,
-                can_pin_messages=not denied_permissions.pin_messages
+                can_pin_messages=not denied_permissions.pin_messages,
+                can_manage_topics=not denied_permissions.manage_topics,
+                can_send_photos=not denied_permissions.send_photos,
+                can_send_videos=not denied_permissions.send_videos,
+                can_send_roundvideos=not denied_permissions.send_roundvideos,
+                can_send_audios=not denied_permissions.send_audios,
+                can_send_voices=not denied_permissions.send_voices,
+                can_send_docs=not denied_permissions.send_docs,
+                can_send_plain=not denied_permissions.send_plain,
             )
+
+    @property
+    def can_send_media_messages(self) -> bool:
+        warn(
+            'This property is deprecated. Use can_send_media instead',
+            DeprecationWarning,
+            stacklevel=2
+        )
+        # DEPRECATED
+        return self.can_send_media
+
+    @property
+    def can_send_other_messages(self) -> bool:
+        warn(
+            'This property is deprecated. '
+            'Use can_send_stickers, can_send_gifs, can_send_games, can_send_inline instead',
+            DeprecationWarning,
+            stacklevel=2
+        )
+        # DEPRECATED
+        return any([
+            self.can_send_stickers,
+            self.can_send_gifs,
+            self.can_send_games,
+            self.can_send_inline
+        ])
+
+    @property
+    def can_add_web_page_previews(self) -> bool:
+        warn(
+            'This property is deprecated. '
+            'Use can_embed_links',
+            DeprecationWarning,
+            stacklevel=2
+        )
+        return self.can_embed_links
