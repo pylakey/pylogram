94e3895cbf16be3ad371e43894963ae08c360e62
pylakey <pylakey@protonmail.com>
2024-01-13 15:09:23 +0300

    Convert Storage class to Abstract Base Class
    In the Storage class within pyrogram/storage/storage.py, the class was converted into an Abstract Base Class (ABC). Each formerly concrete method has been set as an abstract method, enforcing the implementation of these methods in any derived classes. The included methods are critical for managing the storage such as opening, saving, closing, deletion, updating, etc. Also, type hinting has been added to the export_session_string method.

--- pyrogram/storage/storage.py
+++ pyrogram/storage/storage.py
@@ -15,69 +15,84 @@
 #
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
-
+import abc
 import base64
 import struct
-from typing import List, Tuple
+from typing import List
+from typing import Tuple
 
 
-class Storage:
+class Storage(abc.ABC):
     OLD_SESSION_STRING_FORMAT = ">B?256sI?"
     OLD_SESSION_STRING_FORMAT_64 = ">B?256sQ?"
     SESSION_STRING_SIZE = 351
     SESSION_STRING_SIZE_64 = 356
-
     SESSION_STRING_FORMAT = ">BI?256sQ?"
 
     def __init__(self, name: str):
         self.name = name
 
+    @abc.abstractmethod
     async def open(self):
         raise NotImplementedError
 
+    @abc.abstractmethod
     async def save(self):
         raise NotImplementedError
 
+    @abc.abstractmethod
     async def close(self):
         raise NotImplementedError
 
+    @abc.abstractmethod
     async def delete(self):
         raise NotImplementedError
 
+    @abc.abstractmethod
     async def update_peers(self, peers: List[Tuple[int, int, str, str, str]]):
         raise NotImplementedError
 
+    @abc.abstractmethod
     async def get_peer_by_id(self, peer_id: int):
         raise NotImplementedError
 
+    @abc.abstractmethod
     async def get_peer_by_username(self, username: str):
         raise NotImplementedError
 
+    @abc.abstractmethod
     async def get_peer_by_phone_number(self, phone_number: str):
         raise NotImplementedError
 
+    @abc.abstractmethod
     async def dc_id(self, value: int = object):
         raise NotImplementedError
 
+    @abc.abstractmethod
     async def api_id(self, value: int = object):
         raise NotImplementedError
 
+    @abc.abstractmethod
     async def test_mode(self, value: bool = object):
         raise NotImplementedError
 
+    @abc.abstractmethod
     async def auth_key(self, value: bytes = object):
         raise NotImplementedError
 
+    @abc.abstractmethod
     async def date(self, value: int = object):
         raise NotImplementedError
 
+    @abc.abstractmethod
     async def user_id(self, value: int = object):
         raise NotImplementedError
 
+    @abc.abstractmethod
     async def is_bot(self, value: bool = object):
         raise NotImplementedError
 
-    async def export_session_string(self):
+    async def export_session_string(self) -> str:
         packed = struct.pack(
             self.SESSION_STRING_FORMAT,
             await self.dc_id(),
