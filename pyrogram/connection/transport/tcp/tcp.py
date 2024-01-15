45258307511f054b5a1edd586175fe95fb4eaee5
pylakey <pylakey@protonmail.com>
2024-01-15 16:19:03 +0300

    Refactor TCP transport and update dependencies
    The TCP transport was significantly refactored for improved organization and structure. The code handling proxies was simplified and made more straightforward via the usage of the python_socks library. The old way of handling proxies was greatly reduced and replaced by python_socks. This change necessitated an update of 'requirements.txt' to include python-socks version 2.4.4 for asyncio support.

--- pyrogram/connection/transport/tcp/tcp.py
+++ pyrogram/connection/transport/tcp/tcp.py
@@ -18,24 +18,10 @@
 
 import asyncio
 import logging
-import socket
-import time
+from typing import Optional
 
-try:
-    import python_socks
-except ImportError:
-    python_socks = None
-
-try:
-    import socks
-except ImportError as e:
-    if python_socks is None:
-        e.msg = (
-            "PySocks is missing and Pyrogram can't run without. "
-            "Please install it using \"pip3 install pysocks\"."
-        )
-
-        raise e
+from python_socks import ProxyType
+from python_socks.async_.asyncio import Proxy
 
 log = logging.getLogger(__name__)
 
@@ -45,155 +31,69 @@ class TCP:
 
     def __init__(self, ipv6: bool, proxy: dict):
         self.socket = None
-
-        self.reader = None
-        self.writer = None
-
+        self.reader: Optional[asyncio.StreamReader] = None
+        self.writer: Optional[asyncio.StreamWriter] = None
         self.lock = asyncio.Lock()
         self.loop = asyncio.get_event_loop()
-        self.proxy = proxy
+        self.proxy = self._parse_proxy(proxy) if proxy else None
         self.ipv6 = ipv6
 
     @staticmethod
-    def _parse_proxy(scheme, hostname, port, rdns=True, username=None, password=None):
+    def _parse_proxy(proxy_data: Optional[dict]) -> Optional[Proxy]:
+        if proxy_data is None:
+            return None
+
+        if not isinstance(proxy_data, dict):
+            raise TypeError("Proxy must be a dict")
+
+        scheme = proxy_data.get('scheme')
+        hostname = proxy_data.get('hostname')
+        port = proxy_data.get('port')
+        rdns = proxy_data.get('rdns', True)
+        username = proxy_data.get('username', None)
+        password = proxy_data.get('password', None)
+
         if isinstance(scheme, str):
             scheme = scheme.lower()
 
-        # Always prefer `python_socks` when available
-        if python_socks:
-            from python_socks import ProxyType
-
-            # We do the check for numerical values here
-            # to be backwards compatible with PySocks proxy format,
-            # (since socks.SOCKS5 == 2, socks.SOCKS4 == 1, socks.HTTP == 3)
-            if scheme == ProxyType.SOCKS5 or scheme == 2 or scheme == "socks5":
-                protocol = ProxyType.SOCKS5
-            elif scheme == ProxyType.SOCKS4 or scheme == 1 or scheme == "socks4":
-                protocol = ProxyType.SOCKS4
-            elif scheme == ProxyType.HTTP or scheme == 3 or scheme == "http":
-                protocol = ProxyType.HTTP
-            else:
-                raise ValueError("Unknown proxy protocol type: {}".format(scheme))
-
-            # This tuple must be compatible with `python_socks`' `Proxy.create()` signature
-            return protocol, hostname, port, username, password, rdns
-
+        if scheme == ProxyType.SOCKS5 or scheme == 2 or scheme == "socks5":
+            protocol = ProxyType.SOCKS5
+        elif scheme == ProxyType.SOCKS4 or scheme == 1 or scheme == "socks4":
+            protocol = ProxyType.SOCKS4
+        elif scheme == ProxyType.HTTP or scheme == 3 or scheme == "http":
+            protocol = ProxyType.HTTP
         else:
-            from socks import (
-                SOCKS5,
-                SOCKS4,
-                HTTP,
-            )
+            raise ValueError("Unknown proxy protocol type: {}".format(scheme))
 
-            if scheme == 2 or scheme == "socks5":
-                protocol = SOCKS5
-            elif scheme == 1 or scheme == "socks4":
-                protocol = SOCKS4
-            elif scheme == 3 or scheme == "http":
-                protocol = HTTP
-            else:
-                raise ValueError("Unknown proxy protocol type: {}".format(scheme))
-
-            # This tuple must be compatible with `PySocks`' `socksocket.set_proxy()` signature
-            return protocol, hostname, port, rdns, username, password
-
-    async def _proxy_connect(self, dest_ip: str, dest_port: int, timeout: int = TIMEOUT) -> socket.socket:
-        if isinstance(self.proxy, (tuple, list)):
-            parsed = self._parse_proxy(*self.proxy)
-        elif isinstance(self.proxy, dict):
-            parsed = self._parse_proxy(
-                scheme=self.proxy.get('scheme'),
-                hostname=self.proxy.get('hostname'),
-                port=self.proxy.get('port'),
-                rdns=self.proxy.get('rdns', True),
-                username=self.proxy.get('username'),
-                password=self.proxy.get('password'),
-            )
-        else:
-            raise TypeError("Proxy of unknown format: {}".format(type(self.proxy)))
-
-        # Always prefer `python_socks` when available
-        if python_socks:
-            # python_socks internal errors are not inherited from
-            # builtin IOError (just from Exception). Instead of adding those
-            # in exceptions clauses everywhere through the code, we
-            # rather monkey-patch them in place.
-            python_socks._errors.ProxyError = ConnectionError
-            python_socks._errors.ProxyConnectionError = ConnectionError
-            python_socks._errors.ProxyTimeoutError = ConnectionError
-
-            from python_socks.async_.asyncio import Proxy
-
-            proxy = Proxy.create(*parsed)
-
-            sock = await proxy.connect(
-                dest_host=dest_ip,
-                dest_port=dest_port,
-                timeout=timeout
-            )
-
-            log.info(f"Connected to {self.proxy.get('scheme')} proxy {parsed[1]}:{parsed[2]} with python_socks")
-        else:
-            import socks
-
-            # Here `address` represents destination address (not proxy), because of
-            # the `PySocks` implementation of the connection routine.
-            # IPv family is checked on proxy address, not destination address.
-            if self.ipv6:
-                mode, address = socket.AF_INET6, (dest_ip, dest_port, 0, 0)
-            else:
-                mode, address = socket.AF_INET, (dest_ip, dest_port)
-
-            # Setup socket, proxy, timeout and bind it (if necessary).
-            sock = socks.socksocket(mode, socket.SOCK_STREAM)
-            sock.set_proxy(*parsed)
-            sock.settimeout(timeout)
-
-            # Actual TCP connection and negotiation performed here.
-            await asyncio.wait_for(
-                asyncio.get_event_loop().sock_connect(sock=sock, address=address),
-                timeout=timeout
-            )
-
-            log.info(f"Connected to {self.proxy.get('scheme')} proxy {parsed[1]}:{parsed[2]} with PySocks")
-
-        return sock
+        return Proxy(protocol, hostname, port, username=username, password=password, rdns=rdns)
 
     async def connect(self, address: tuple):
         (ip_addr, port) = address
 
-        if self.proxy:
-            self.socket = await self._proxy_connect(ip_addr, port, timeout=TCP.TIMEOUT)
+        if isinstance(self.proxy, Proxy):
+            self.socket = await self.proxy.connect(dest_host=ip_addr, dest_port=port, timeout=TCP.TIMEOUT)
+            log.info(f"Successfully connected to proxy {self.proxy.proxy_host}:{self.proxy.proxy_port}")
             self.reader, self.writer = await asyncio.wait_for(
                 asyncio.open_connection(sock=self.socket),
-                timeout=TCP.TIMEOUT
+                timeout=TCP.TIMEOUT,
             )
         else:
             self.reader, self.writer = await asyncio.wait_for(
-                asyncio.open_connection(
-                    host=ip_addr,
-                    port=port,
-                ),
+                asyncio.open_connection(host=ip_addr, port=port),
                 timeout=TCP.TIMEOUT
             )
 
         await self.writer.drain()
 
     async def close(self):
-        try:
+        if self.writer is not None and not self.writer.is_closing():
             self.writer.close()
-            await asyncio.wait_for(self.writer.wait_closed(), timeout=10)
-        except AttributeError as e:
-            if bool(self.socket):
-                try:
-                    self.socket.shutdown(socket.SHUT_RDWR)
-                except OSError:
-                    pass
-                finally:
-                    # A tiny sleep placed here helps avoiding .recv(n) hanging until the timeout.
-                    # This is a workaround that seems to fix the occasional delayed stop of a client.
-                    time.sleep(0.001)
-                    self.socket.close()
+            try:
+                await asyncio.wait_for(self.writer.wait_closed(), timeout=TCP.TIMEOUT)
+            except (asyncio.TimeoutError, TimeoutError):
+                log.warning('Graceful disconnection timed out, forcibly ignoring cleanup')
+            except BaseException as e:
+                log.warning('%s during disconnect: %s', type(e).__name__, e)
 
     async def send(self, data: bytes):
         async with self.lock:
@@ -201,11 +101,11 @@ class TCP:
                 if self.writer is not None:
                     self.writer.write(data)
                     await self.writer.drain()
-            except Exception as e:
+            except BaseException as e:
                 log.info("Send exception: %s %s", type(e).__name__, e)
-                raise OSError(e)
+                raise OSError(e) from e
 
-    async def recv(self, length: int = 0):
+    async def recv(self, length: int = 0) -> Optional[bytes]:
         data = b""
 
         while len(data) < length:
