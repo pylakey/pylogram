64f8f4180cf36289850c95bd320d9809c7773484
Pylakey <pylakey@protonmail.com>
2024-02-25 16:32:20 +0300

    Use TCPFull as connection protocol

--- pylogram/connection/connection.py
+++ pylogram/connection/connection.py
@@ -21,7 +21,8 @@ import asyncio
 import logging
 from typing import Optional
 
-from .transport import TCP, TCPAbridged
+from .transport import TCP
+from .transport import TCPFull
 from ..session.internals import DataCenter
 
 log = logging.getLogger(__name__)
@@ -42,7 +43,7 @@ class Connection:
 
     async def connect(self):
         for i in range(Connection.MAX_CONNECTION_ATTEMPTS):
-            self.protocol = TCPAbridged(self.ipv6, self.proxy)
+            self.protocol = TCPFull(self.ipv6, self.proxy)
 
             try:
                 log.info("Connecting...")
