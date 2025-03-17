3b13dd723f499ca1796dc2b11940e6f21a4f49bd
Pylakey <pylakey@protonmail.com>
2025-03-17 15:20:53 +0300

    Update hatch_build.py to restrict compilation to 'install' target only; add logging for compilation steps. Increment version to 0.11.6 in __init__.py for release.

--- hatch_build.py
+++ hatch_build.py
@@ -9,11 +9,16 @@ sys.path.insert(0, ".")
 class CustomHook(BuildHookInterface):
     def initialize(self, version, build_data):
         """Initialize the hook."""
-        if self.target_name not in {"wheel", "install"}:
+        if self.target_name in {"install"}:
             return
 
         from compiler.api.compiler import start as compile_api
         from compiler.errors.compiler import start as compile_errors
 
+        print("compiling api")
         compile_api()
+        print("compiled api")
+
+        print("compiling errors")
         compile_errors()
+        print("compiled errors")
